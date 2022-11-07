6f9050968288694adf550c75759d01d7191ff3dc
Juvencio Linhares <juvlinhares@gmail.com>
2022-11-07 00:15:43 -0300

    adicionado flap e rotação

--- src/Passaro.java
+++ src/Passaro.java
@@ -5,11 +5,11 @@ public class Passaro {
 	public double x, y;
 	
 	//velocidade
-	public double velocidadeY;
+	public double velocidadey = 0;
 	
 	//constantes:
 	
-	public static double GRAVIDADE = 500;
+	public static double GRAVIDADE = 800;
 	public static double FLAP = - 300;
 	
 	
@@ -19,11 +19,23 @@ public class Passaro {
 		this.y = y;
 	}
 
+	//movimento do passaro no eixo y
+	public void atualiza(double dt) {
+		velocidadey += GRAVIDADE * dt;
+		y += velocidadey * dt;
 
+	}
 
-	//desenhar o pássaro:
+	//função que anula a queda quando chamada/ativada, faz o passaro subir  
+	public void flap() {
+	
+		velocidadey = FLAP;
+	}
+	
+
+	//desenhar o pássaro/ rotação do pássaro( Math.atan):
 	public void desenhar(Tela t) {
-		t.imagem("flappy.png", 528, 128, 34, 24, 0, x, y);
+		t.imagem("flappy.png", 528, 128, 34, 24, Math.atan(velocidadey / 300), x, y);
 	}
 
 }
